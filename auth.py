40ae1fb8b269917a154002148576ebcf07aec58c
Chen Zheng <nkchenz@gmail.com>
2010-04-23 20:58:42 +0800

    update auth module

--- auth.py
+++ auth.py
@@ -1,45 +1,66 @@
-"""Common login module for web form """
+"""Web form authentication helper
+
+Example:
+
+Edit your config file to suit the login form of your web site, like this:
+
+    {"action": "https://www.xxxx.com/login",
+    "username": "zhengchen",
+    "password": "*******",
+    "url": "https://www.xxx.com/index"}
+
+'url' is an example for additional informations required by server, you
+should put all of them here.
+And the code:
+
+    import auth
+    test = auth.AuthenticationHelper('test.auth')
+    test.login()
+    opener = test.get_opener()
+"""
 
 import os
 import urllib2
 import urllib
+import urlparse
 import cookielib
-from getpass import getpass
+import json
 
-class Auth:
+class AuthenticationHelper:
 
-    def __init__(self, **paras):
-        """ Paras are:
-            @cookie_file
-            @action         post action of login form
-            @user_label     user name label in login form
-            @passwd_label
-            @other_data     data hidden in login form
+    def __init__(self, config):
+        """ File 'config' is in json format, contains the informations about
+        the login form.
         """
-        self.paras = paras
-        if 'other_data' not in self.paras:
-            self.paras['other_data'] = {}
+        if not os.path.exists(config):
+            raise IOError('Config file required')
+        self.paras = json.load(open(config, 'r'))
+
+        url = urlparse.urlparse(self.paras['action'])
+        tmp = url.netloc + os.path.join(url.path, 'cookie').replace('/', '.')
+        self.cookie_file = os.path.join(os.path.expanduser('~/.' + tmp))
+
+        self.cj = cookielib.LWPCookieJar()
 
     def login(self):
-        cj = cookielib.LWPCookieJar()
-        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj), urllib2.HTTPSHandler)
-        user = raw_input('User: ')
-        passwd = getpass()
-        data = {self.paras['user_label']: user, self.paras['passwd_label']: passwd}
-        data.update(self.paras['other_data'])
-        data = urllib.urlencode(data)
-        page = opener.open(self.paras['action'], data)
-    
+        """Perform the login and save cookies for further use """
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cj), urllib2.HTTPSHandler)
+        page = opener.open(self.paras['action'], urllib.urlencode(self.paras))
+
         # Fixme! Should detect if we're login ok
         # if not self.check_login_callback():
         #   return False
-        cj.save(self.paras['cookie_file'], ignore_discard = True, ignore_expires= True) # Save cookie
-        os.chmod(self.paras['cookie_file'], 0600)
+
+        self.cj.save(self.cookie_file, ignore_discard = True, ignore_expires= True)
+        print 'Login ok'
         return True
 
     def get_opener(self):
-        cj = cookielib.LWPCookieJar()
-        if not os.path.isfile(self.paras['cookie_file']):
-            return None
-        cj.load(self.paras['cookie_file'], ignore_discard = True, ignore_expires= True)
-        return urllib2.build_opener(urllib2.HTTPCookieProcessor(cj), urllib2.HTTPSHandler)
+        """Return an urllib2.OpenerDirector object that you can play with"""
+        if not os.path.exists(self.cookie_file):
+            return None # Not login
+        self.cj.load(self.cookie_file, ignore_discard = True, ignore_expires= True)
+        return urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cj), urllib2.HTTPSHandler)
+
+if __name__ == '__main__':
+    pass
